d14cc8caab3919da4f176b99f9555b3eb7d75cc4
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-08-16 07:00:15 -0700

    vercel.json configured

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ import express from "express";
 export const app = express();
 import cookieParser from "cookie-parser";
 import { userRouter } from "./routes/User.js";
+import cors from 'cors'
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "l"
+  origin: "localhost:3000"
 }))
 
 app.use("/api/v1", userRouter);
