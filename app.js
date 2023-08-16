2207723f166d5de19304dca8083dc5edd10614b9
mohammadtahirsultan <tahirsultanofficial@gmail.com>
2023-08-16 09:23:26 -0700

    vercel.json configured

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ import cookieParser from "cookie-parser";
 import { userRouter } from "./routes/User.js";
 import cors from 'cors'
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://3d-mern-portfolio.netlify.app"
+  origin: "https://3d-portfolio-mern.vercel.app"
 }))
 
 app.use("/api/v1", userRouter);
